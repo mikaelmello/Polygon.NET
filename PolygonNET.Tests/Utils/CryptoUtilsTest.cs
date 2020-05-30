dfba53ca4643e92628e89f1f8126fad6f1d3c468
Mikael Mello <git@mikaelmello.com>
2020-05-30 19:42:19 -0300

    Add ICryptoUtils interface for future DI

--- PolygonNET.Tests/Utils/CryptoUtilsTest.cs
+++ PolygonNET.Tests/Utils/CryptoUtilsTest.cs
@@ -2,7 +2,16 @@ using NUnit.Framework;
 using PolygonNET.Utils;
 
 namespace PolygonNET.Tests.Utils {
+    
+    [TestFixture]
     public class CryptoUtilsTest {
+        private ICryptoUtils _cryptoUtils;
+
+        [OneTimeSetUp]
+        public void Setup() {
+            _cryptoUtils = new CryptoUtils();
+        }
+        
         [Test]
         public void LoremIpsumHashMatches() {
             // ReSharper disable StringLiteralTypo
@@ -14,7 +23,7 @@ namespace PolygonNET.Tests.Utils {
                                 "deserunt mollit anim id est laborum.";
             const string expectedHash = "8BA760CAC29CB2B2CE66858EAD169174057AA1298CCD581514E6DB6DEE3285280EE6E3A54C" +
                                         "9319071DC8165FF061D77783100D449C937FF1FB4CD1BB516A69B9";
-            var calculatedHash = CryptoUtils.ComputeSha512Hash(text);
+            var calculatedHash = _cryptoUtils.ComputeSha512Hash(text);
 
             Assert.AreEqual(expectedHash, calculatedHash);
         }
@@ -25,7 +34,7 @@ namespace PolygonNET.Tests.Utils {
             const string text = "<rand>/<methodName>?param1=value1&param2=value2...&paramN=valueN#<secret>";
             const string expectedHash = "2A47D4BE55EFE9CF64C8FDB16C748640C49398A3768B082DA73CE61CD9CDB37A27A9D184" +
                                         "3678B2EBE94A0083AAE5686DADC713D812AF0C3CA469CF2BFEB6DF5C";
-            var calculatedHash = CryptoUtils.ComputeSha512Hash(text);
+            var calculatedHash = _cryptoUtils.ComputeSha512Hash(text);
 
             Assert.AreEqual(expectedHash, calculatedHash);
         }
--- PolygonNET/Utils/CryptoUtils.cs
+++ PolygonNET/Utils/CryptoUtils.cs
@@ -3,8 +3,19 @@ using System.Security.Cryptography;
 using System.Text;
 
 namespace PolygonNET.Utils {
-    public static class CryptoUtils {
-        public static string ComputeSha512Hash(string plainText) {
+    public interface ICryptoUtils {
+        /// <summary>
+        /// Computes the SHA512 hash of <paramref name="plainText"/> and returns its equivalent hexadecimal
+        /// representation, uppercase and stripped of dashes.
+        /// </summary>
+        /// <param name="plainText">Text to be hashed</param>
+        /// <returns>Equivalent hexadecimal representation of the SHA512 hash of <paramref name="plainText"/>,
+        /// uppercase and stripped of dashes.</returns>
+        public string ComputeSha512Hash(string plainText);
+    }
+    
+    public class CryptoUtils : ICryptoUtils {
+        public string ComputeSha512Hash(string plainText) {
             using var sha512 = new SHA512Managed();
             var hash = sha512.ComputeHash(Encoding.UTF8.GetBytes(plainText));
             return BitConverter.ToString(hash).Replace("-", "");
