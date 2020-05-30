765568cbfb3bbeccb85a357beb4c0837300b985c
Mikael Mello <git@mikaelmello.com>
2020-05-30 19:44:36 -0300

    Add IPolygonAuth interface for future DI and more tests for the implementation

--- PolygonNET.Tests/Network/PolygonAuthTest.cs
+++ PolygonNET.Tests/Network/PolygonAuthTest.cs
@@ -1,39 +1,97 @@
 using System;
 using System.Collections.Generic;
+using System.Security.Cryptography;
+using System.Text;
 using Bogus;
+using Moq;
 using NUnit.Framework;
 using PolygonNET.Network;
+using PolygonNET.Utils;
 
 namespace PolygonNET.Tests.Network {
     public class PolygonAuthTest {
+        private Mock<IRandomUtils> _randomUtils;
+        private Mock<ICryptoUtils> _cryptoUtils;
+        private IPolygonAuth _polygonAuth;
         private Faker _faker;
 
-        [SetUp]
+        [OneTimeSetUp]
         public void Setup() {
             _faker = new Faker();
+        }
+
+        [SetUp]
+        public void BeforeEach() {
+            _randomUtils = new Mock<IRandomUtils> {CallBase = true};
+            _cryptoUtils = new Mock<ICryptoUtils> {CallBase = true};
+            _polygonAuth = new PolygonAuth(_randomUtils.Object, _cryptoUtils.Object);
         }
 
         [Test]
         public void AuthorizeRequestCreatesCorrectParams() {
             var methodName = _faker.Internet.UserName();
+            var apiKey = _faker.Random.Guid().ToString();
+            var apiSecret = _faker.Random.Guid().ToString();
+            var time = DateTimeOffset.Now.ToUnixTimeMilliseconds();
+            var expectedSignature = _faker.Random.Hexadecimal(128);
+            _cryptoUtils.Setup(c => c.ComputeSha512Hash(It.IsAny<string>()))
+                        .Returns(expectedSignature);
+
             var parameters = new Dictionary<string, string> {
                 {"key1", "value1"},
                 {"key2", "value2"}
             };
-            var apiKey = _faker.Random.Guid().ToString();
-            var apiSecret = _faker.Random.Guid().ToString();
-            var time = DateTimeOffset.Now.ToUnixTimeMilliseconds();
-
-            var authParams = PolygonAuth.AuthorizeRequest(methodName, parameters, apiKey, apiSecret);
-            var paramTime = long.Parse(authParams["time"]);
-
+            
+            _polygonAuth.AuthorizeRequest(methodName, parameters, apiKey, apiSecret);
+            
+            var paramTime = long.Parse(parameters["time"]);
             var diffTime = (time - paramTime);
-
-            Assert.AreEqual(apiKey, authParams["apiKey"]);
-            Assert.AreEqual("value1", authParams["key1"]);
-            Assert.AreEqual("value2", authParams["key2"]);
-            Assert.True(authParams.ContainsKey("apiSig"));
             Assert.LessOrEqual(diffTime, 1000 * 60 * 5);
+            
+            Assert.AreEqual(apiKey, parameters["apiKey"]);
+            Assert.AreEqual("value1", parameters["key1"]);
+            Assert.AreEqual("value2", parameters["key2"]);
+            Assert.AreEqual(expectedSignature, parameters["apiSig"]);
+        }
+
+        [Test]
+        public void GetApiSignatureCreatesCorrectHash() {
+            _randomUtils.Setup(r => r.GetRandomAlphanumericString(It.IsAny<int>()))
+                        .Returns("qwerty");
+            _cryptoUtils.Setup(c => c.ComputeSha512Hash(It.IsAny<string>()))
+                        .Returns<string>(Sha512);
+
+            var apiSecret = _faker.Random.Hexadecimal(128);
+            var methodName = _faker.Random.String2(12);
+            var parameters = new Dictionary<string, string> {
+                {"key1", "value1"},
+                {"key2", "value2"},
+                {"abc", "def"},
+            };
+            var expected = Sha512($"qwerty/{methodName}?abc=def&key1=value1&key2=value2#{apiSecret}");
+
+            var apiSignature = _polygonAuth.GetApiSignature(methodName, parameters, apiSecret);
+
+            _randomUtils.Verify(r => r.GetRandomAlphanumericString(It.IsAny<int>()), Times.Once);
+            _cryptoUtils.Verify(c => c.ComputeSha512Hash(It.IsAny<string>()), Times.Once);
+
+            Assert.AreEqual(expected, apiSignature);
+        }
+
+        [Test]
+        public void GetApiSignatureUsesCorrectRandLen() {
+            const int expectedRandLen = 6;
+            
+            _ = _polygonAuth.GetApiSignature(default, new Dictionary<string, string>(), default);
+
+            _randomUtils.Verify(r => r.GetRandomAlphanumericString(It.IsAny<int>()), Times.Once);
+            _randomUtils.Verify(r => r.GetRandomAlphanumericString(expectedRandLen), Times.Once);
+        }
+
+        private static string Sha512(string plainText) {
+            using var sha512 = new SHA512Managed();
+            var hash = sha512.ComputeHash(Encoding.UTF8.GetBytes(plainText));
+            return BitConverter.ToString(hash).Replace("-", "");
         }
     }
 }
--- PolygonNET/Network/PolygonAuth.cs
+++ PolygonNET/Network/PolygonAuth.cs
@@ -4,25 +4,53 @@ using System.Linq;
 using PolygonNET.Utils;
 
 namespace PolygonNET.Network {
-    public static class PolygonAuth {
-        public static Dictionary<string, string> AuthorizeRequest(string methodName,
-                                                                  Dictionary<string, string> parameters,
-                                                                  string apiKey, string apiSecret) {
+    public interface IPolygonAuth {
+        /// <summary>
+        /// Adds the apiKey, time and apiSig parameters in the <paramref name="parameters"/> dictionary, necessary
+        /// for auth in the Polygon API.
+        /// </summary>
+        /// <param name="methodName">Name of the method called by the request.</param>
+        /// <param name="parameters">Parameters containing in the request.</param>
+        /// <param name="apiKey">Polygon API Key</param>
+        /// <param name="apiSecret">Polygon API Secret</param>
+        public void AuthorizeRequest(string methodName, Dictionary<string, string> parameters, string apiKey,
+                                     string apiSecret);
+
+        /// <summary>
+        /// Returns the signature needed for auth in the Polygon API, based on the <paramref name="methodName"/>,
+        /// <paramref name="parameters"/> and <paramref name="apiSecret"/>.
+        /// </summary>
+        /// <param name="methodName">Name of the method called by the request.</param>
+        /// <param name="parameters">Parameters containing in the request.</param>
+        /// <param name="apiSecret">Polygon API Secret</param>
+        public string GetApiSignature(string methodName, Dictionary<string, string> parameters, string apiSecret);
+    }
+
+    public class PolygonAuth : IPolygonAuth {
+        private readonly IRandomUtils _randomUtils;
+        private readonly ICryptoUtils _cryptoUtils;
+
+        public PolygonAuth(IRandomUtils randomUtils, ICryptoUtils cryptoUtils) {
+            _randomUtils = randomUtils;
+            _cryptoUtils = cryptoUtils;
+        }
+
+        public void AuthorizeRequest(string methodName,
+                                     Dictionary<string, string> parameters,
+                                     string apiKey, string apiSecret) {
             parameters["apiKey"] = apiKey;
             parameters["time"] = DateTimeOffset.Now.ToUnixTimeMilliseconds().ToString();
             parameters["apiSig"] = GetApiSignature(methodName, parameters, apiSecret);
-
-            return parameters;
         }
 
-        private static string GetApiSignature(string methodName, Dictionary<string, string> parameters,
-                                              string apiSecret) {
-            var rand = RandomUtils.GetRandomAlphanumericString(6);
+        public string GetApiSignature(string methodName, Dictionary<string, string> parameters,
+                                      string apiSecret) {
+            var rand = _randomUtils.GetRandomAlphanumericString(6);
 
             var orderedParams = parameters.OrderBy(pair => pair.Key);
             var queryString = orderedParams.BuildQueryString();
             var plainText = $"{rand}/{methodName}?{queryString}#{apiSecret}";
-            var hash = CryptoUtils.ComputeSha512Hash(plainText);
+            var hash = _cryptoUtils.ComputeSha512Hash(plainText);
             return hash;
         }
     }
