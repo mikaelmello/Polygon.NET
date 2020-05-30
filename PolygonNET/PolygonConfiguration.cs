33b0deb6bdcfffb8cb3321d1ba6d6b9351139b08
Mikael Mello <git@mikaelmello.com>
2020-05-30 19:48:10 -0300

    Add PolygonConfiguration

---
+++ PolygonNET/PolygonConfiguration.cs
@@ -0,0 +1,7 @@
+namespace PolygonNET {
+    public class PolygonConfiguration {
+        public string ApiKey { get; set; }
+        public string ApiSecret { get; set; }
+        public string ApiBaseUrl { get; set; } = "https://polygon.codeforces.com/api/";
+    }
+}
