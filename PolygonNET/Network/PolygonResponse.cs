9c16fa75d0d2bb214ab3436fdfae9f896974ad35
Mikael Mello <git@mikaelmello.com>
2020-05-31 23:45:53 -0300

    Add documentation for PolygonResponse

--- PolygonNET/Network/PolygonResponse.cs
+++ PolygonNET/Network/PolygonResponse.cs
@@ -2,6 +2,9 @@ using Newtonsoft.Json;
 using Newtonsoft.Json.Converters;
 
 namespace PolygonNET.Network {
+    /// <summary>
+    /// Possible response status of API requests.
+    /// </summary>
     [JsonConverter(typeof(StringEnumConverter))]
     public enum PolygonResponseStatus {
         Ok,
@@ -9,13 +12,30 @@ namespace PolygonNET.Network {
     }
 
     public sealed class PolygonResponse<T> {
+        /// <summary>
+        /// Status of the response. <see cref="PolygonResponseStatus.Ok"/> if the request has succeeded,
+        /// <see cref="PolygonResponseStatus.Failed"/> if not.
+        /// </summary>
         public PolygonResponseStatus Status { get; set; }
+        /// <summary>
+        /// If the request has failed, it contains the reason, null otherwise. 
+        /// </summary>
         public string Comment { get; set; }
+        /// <summary>
+        /// Result of the request.
+        /// </summary>
         public T Result { get; set; }
     }
 
     public sealed class PolygonFailedResponse {
+        /// <summary>
+        /// Status of the response. <see cref="PolygonResponseStatus.Ok"/> if the request has succeeded,
+        /// <see cref="PolygonResponseStatus.Failed"/> if not.
+        /// </summary>
         public PolygonResponseStatus Status { get; set; }
+        /// <summary>
+        /// If the request has failed, it contains the reason, null otherwise. 
+        /// </summary>
         public string Comment { get; set; }
     }
 }
